ae027c57f1f6f99ac78934a1efe77b02f3a19d95
Shaix33 <KevinNdlondlo@gmail.com>
2022-10-16 14:54:29 -0700

    adding my files

--- 0-printf.c
+++ 0-printf.c
@@ -5,4 +5,22 @@
  * _printf: a function that produces output according to a format
  * description: custom printf function
  *
- * @format: pointer to 
+ * @format: pointer to string arguments
+ * Return: the number of characters printed excluding the null byte
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int i;
+	
+	va_start(args, count);
+
+	for (i = 0; i < count; i++)
+	{
+		i++;
+	}
+	return (i);
+}
+		
