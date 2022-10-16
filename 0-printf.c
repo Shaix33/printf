2239607a917aa129bf9c0fae73a40bd9a0245477
Shaix33 <KevinNdlondlo@gmail.com>
2022-10-16 11:45:15 -0700

    Updating readme

---
+++ 0-printf.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf: a function that produces output according to a format
+ * description: custom printf function
+ *
+ * @format: pointer to 
---
+++ test/README.md
@@ -0,0 +1,1 @@
+## [Creating a custom printf function](https://www.academia.edu/10297206/Secrets_of_printf_)
