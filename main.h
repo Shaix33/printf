e356e3bcd82b6a7e159441119f4b5f70584023e0
khumilla <hold.the.dianhua@gmail.com>
2022-10-17 22:44:17 +0000

    _printf

--- main.h
+++ main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+va_list list, char buffer[], int flags, int width, int precision, int size;
 
-#endif
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+
